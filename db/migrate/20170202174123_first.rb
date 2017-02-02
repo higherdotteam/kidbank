c01a9c9aa2835ba308b724446088972e0bc73152
A Arrow <oneone@gmail.com>
2017-02-02 17:51:20 +0000

    accounts

--- db/migrate/20170202174123_first.rb
+++ db/migrate/20170202174123_first.rb
@@ -3,17 +3,38 @@ class First < ActiveRecord::Migration
     create_table :kids do |t|
       t.string :fname
       t.string :lname
+      t.string :email
       t.datetime :dob
     end
 
     create_table :parents do |t|
       t.string :fname
       t.string :lname
+      t.string :email
     end
 
     create_table :kid_parents do |t|
       t.integer :kid_id
       t.integer :parent_id
+    end
+
+    create_table :accounts do |t|
+      t.integer :kid_id
+      t.string :flavor
+      t.integer :balance
+    end
+
+    create_table :activities do |t|
+      t.integer :account_id
+      t.integer :amount
+      t.string :action
+    end
+
+    create_table :admins do |t|
+      t.string :fname
+      t.string :lname
+      t.string :email
+      t.integer :level, default: 1
     end
   end
 end
