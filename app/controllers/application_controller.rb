1fc8ef6606da2bc251c76f349e129b2138450a1f
A Arrow <oneone@gmail.com>
2017-02-03 04:32:54 +0000

    current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_user
+
+  def current_user
+    pid = session[:person_id]
+    return nil if pid == nil
+    @current_user ||= Kid.find_by_id(pid)
+  end
 end
