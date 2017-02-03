0b12fe0474ae95c2169f16ba4f6152c3aa9eeec4
A Arrow <oneone@gmail.com>
2017-02-03 18:56:04 +0000

    tokens

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,4 +1,17 @@
 class Api::V1::ApiController < ApplicationController
+  # curl -H "Authorization: Bearer AB467-1390" http://127.0.0.1:3000/api/v1/accounts
+  before_action :check_token
+
+  def check_token
+    token = request.headers['Authorization'].split(" ").last
+    t=Token.where(token: token).first
+    unless t
+      render :text => 'token missing', status: 403
+      return false
+    end
+    @user = t.customer
+  end
+  
   def accounts
     render :json => {"results": [1,2,3]}
   end
