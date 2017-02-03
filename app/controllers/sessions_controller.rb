3dd88b2c466d1f771dc169a8f1e4910af324e6b5
Oscar Luza <onluza@gmail.com>
2017-02-03 01:12:34 -0500

    logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,10 @@ class SessionsController < ApplicationController
     session[:person_id] = Kid.first.id
     redirect_to '/'
   end
+
+  def destroy
+    session[:person_id] = nil
+    redirect_to root_path
+  end
+
 end
