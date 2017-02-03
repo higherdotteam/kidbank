cb57d21ff9b9b1a87b620bd463dee692ef264e2d
A Arrow <oneone@gmail.com>
2017-02-03 07:15:30 +0000

    links

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,16 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Customer.delete_all
-Customer.create(fname: 'Jackson', lname: 'Arrow', dob: 7.years.ago, email: 'jsarrow@kid.org', password: '123')
-Customer.create(fname: 'Jason', lname: 'Arrow', dob: 4.years.ago, email: 'jgarrow@kid.org', password: '123')
-Customer.create(fname: 'Athena', lname: 'Arrow', dob: 1.years.ago, email: 'aarrow@kid.org', password: '123')
+k1=Customer.create(fname: 'Jackson', lname: 'Arrow', dob: 7.years.ago, email: 'jsarrow@kid.org', password: '123')
+k2=Customer.create(fname: 'Jason', lname: 'Arrow', dob: 4.years.ago, email: 'jgarrow@kid.org', password: '123')
+k3=Customer.create(fname: 'Athena', lname: 'Arrow', dob: 1.years.ago, email: 'aarrow@kid.org', password: '123')
 
 Account.delete_all
-Account.create(flavor: 'checkings', balance: 100.00, kid_id: Kid.first.id)
-Account.create(flavor: 'savings', balance: 1000.00, kid_id: Kid.first.id)
+Account.create(flavor: 'checkings', balance: 100.00, kid_id: k1.id)
+Account.create(flavor: 'savings', balance: 1000.00, kid_id: k1.id)
 
-Customer.create(fname: 'Andrew', lname: 'Arrow', dob: 40.years.ago, email: 'andrew@higher.team', password: '123')
+p1=Customer.create(fname: 'Andrew', lname: 'Arrow', dob: 40.years.ago, email: 'andrew@higher.team', password: '123')
+
+KidGrownup.create(kid_id: k1.id, grownup_id: p1.id)
+KidGrownup.create(kid_id: k2.id, grownup_id: p1.id)
+KidGrownup.create(kid_id: k3.id, grownup_id: p1.id)
