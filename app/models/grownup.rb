ed49e0604e30f046126c130a0fe803bf7b9a9384
Andrew Arrow <oneone@gmail.com>
2017-02-05 23:46:04 +0000

    mm

--- app/models/grownup.rb
+++ app/models/grownup.rb
@@ -1,2 +1,12 @@
 class Grownup < Customer
+  before_validation :make_pass
+
+  def make_pass
+
+    words = %W{Bat Bird Bunny Cat Chicken Crab Dog Ducks Elephant Fish Frog Giraffe Hedgehog Jellyfish Leopard 
+     Monkey Moose Mouse Octopus Owl Panda Penguin Pig Rat Rabbit Reindeer Seahorse Sea Urchin
+     Snake Sheep Starfish Tadpole Tiger Turkey Turtle Tortoise Zebra}
+
+    a=words[rand(words.size)].downcase+(rand(9)+1).to_s
+  end
 end
