38396a1b0490ab0338e45dbecf419e0e3596dfa1
A Arrow <oneone@gmail.com>
2017-02-06 04:33:01 +0000

    mm

--- config/initializers/bank_terms.rb
+++ config/initializers/bank_terms.rb
@@ -1,33 +1,47 @@
-COMMON_BILLS = %W{
- Rent
- Mortgage
- Homeowner’s Insurance
- Renter's Insurance
- Home Warranty
- Home Maintenance
- Home Repairs
- Lawn Care
- Property Taxes
- Car Payment
- Car Fuel
- Auto Insurance
- Car Tires
- DMV Registration
- Electric Bill
- Water Bill
- Gas Bill
- Phone Bill
- Internet Service
- Cable TV 
- Satellite TV
- Groceries
- Dining out
- Clothing
- Hair Care
- Medical Expenses
- Gym Membership
- Vacation
- Charitable Giving
- Entertainment
- Gifts
-}
+COMMON_DEALS = [
+  'Stock in KB'
+  'Rare Painting',
+  'Prize Race Horse',
+  'New Computer',
+  'Big Screen TV',
+  'Telsa']
+
+COMMON_INCOME = [ 
+  'Law Suit Settlement',
+  'Stock Dividend',
+  'Friend Repays Old Loan',
+  'Insurance Overpayment',
+  'Liquitity Event']
+
+COMMON_BILLS = [
+ 'Rent',
+ 'Mortgage',
+ "Homeowner’s Insurance",
+ "Renter's Insurance",
+ 'Home Warrantyr',
+ 'Home Maintenance',
+ 'Home Repairs',
+ 'Lawn Care',
+ 'Property Taxes',
+ 'Car Payment',
+ 'Car Fuel',
+ 'Auto Insurance',
+ 'Car Tires',
+ 'DMV Registration',
+ 'Electric Bill',
+ 'Water Bill',
+ 'Gas Bill',
+ 'Phone Bill',
+ 'Internet Service',
+ 'Cable TV ',
+ 'Satellite TV',
+ 'Groceries',
+ 'Dining out',
+ 'Clothing',
+ 'Hair Care',
+ 'Medical Expenses',
+ 'Gym Membership',
+ 'Vacation',
+ 'Charitable Giving',
+ 'Entertainment',
+ 'Gifts']
