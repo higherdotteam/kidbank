3addcc89fdd713bd4ee710db5bdadaf48bc94905
A Arrow <oneone@gmail.com>
2017-02-06 07:34:43 +0000

    mm

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -4,7 +4,38 @@ class CardsController < ApplicationController
     c = Card.find(params[:id])
     if c.flavor == 'bill'
       c.transaction do
-        c.customer.checking += c.amount
+        if params[:source] == 'c'
+          c.customer.checking += c.amount
+        elsif params[:source] == 's'
+          c.customer.savings += c.amount
+        elsif params[:source] == 'l'
+          c.customer.loan += c.amount
+        end
+        c.destroy
+        c.customer.save
+      end
+    elsif c.flavor == 'deal'
+      c.transaction do
+        # TODO add asset
+        if params[:source] == 'c'
+          c.customer.checking += c.amount
+        elsif params[:source] == 's'
+          c.customer.savings += c.amount
+        elsif params[:source] == 'l'
+          c.customer.loan += c.amount
+        end
+        c.destroy
+        c.customer.save
+      end
+    elsif c.flavor == 'check'
+      c.transaction do
+        if params[:source] == 'c'
+          c.customer.checking += c.amount
+        elsif params[:source] == 's'
+          c.customer.savings += c.amount
+        elsif params[:source] == 'l'
+          c.customer.loan += c.amount
+        end
         c.destroy
         c.customer.save
       end
