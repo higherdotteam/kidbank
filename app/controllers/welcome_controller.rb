8bb24fb8f301039dff2ecf25042cbbb83683f535
Andrew Arrow <oneone@gmail.com>
2017-02-06 03:53:39 +0000

    mm

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,13 @@
 class WelcomeController < ApplicationController
   def index
     @kid = Kid.new(dob: 5.years.ago)
+
+    if current_user
+      if current_user.under_13?
+        if current_user.last_rolled == nil || (current_user.last_rolled.day != Time.now.day)
+          current_user.update_attributes(last_rolled: Time.now)
+        end
+      end
+    end
   end
 end
