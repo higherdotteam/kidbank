f19147e67d7e3193958336c6397c8487f67fd912
Andrew Arrow <oneone@gmail.com>
2017-02-06 04:02:17 +0000

    m

--- config/initializers/bank_terms.rb
+++ config/initializers/bank_terms.rb
@@ -1,96 +1,33 @@
-BANK_TERMS = %W{Account
-Adjustment Credit
-Adversely Classified Asset
-Agricultural Sector Investment Program
-American Bankers Association
-Article XII Company
-Automatic Transfer Service
-Availability Float
-Average Collected Balance
-Back-to-Back Commitment
-Balance Reporting
-Bank Credit
-Bank Examination
-Bank Holiday
-Bank Insurance
-Bank Investment Contract
-Bank Marketing Association
-Banknote
-Bank of First Deposit
-Bank Run
-Banks For Cooperatives
-Bank Statement
-Big Six Banks
-Bilateral Credit Limit
-Bond Trustee
-Book Balance
-Branch Automation
-Broad Liquidity
-Brokered Deposit
-Business Banking
-Cashiers Check
-Certificate Of Deposit
-Check Clearing For The 21st Century Act
-Check Safekeeping
-Cleared Funds
-Commercial
-Commercial Account
-Commercial Bank
-Committee On Payment And Settlement Systems
-Compliance Examination
-Concentration Bank
-Conditional Sales Agreement
-Consortium Bank
-Consumer And Business Lending Initiative
-Consumer Bankers Association
-Corporate Agent
-Corporate Trade Payment
-Correspondent Bank
-Cost Of Funds
-Country Limit
-Credit Netting
-Cross-Border Financing
-Dealer Bank
-Deferred Availability
-Delayed Disbursement
-Depository Transfer Check
-Dual Banking System
-Electronic Check
-Electronic Money
-Error Resolution
-Eurobank
-Excess Reserves
-Expedited Funds Availability Act
-Export Trading Company
-Facility
-FDIC Problem Bank List
-Federal Discount Rate
-Federal Funds
-Federal Savings and Loan
-Financial Institution
-Floating Lien
-Foreign Deposits
-Foreign Draft
-Frozen Account
-Funds Management
-Funds Transfer Pricing
-Future Dating
-Glass-Steagall Act
-Home Banking
-Independent Community Bankers Of America
-Insider Lending
-Instructing Bank
-Insured Financial Institution
-Interbank Deposits
-Irrevocable Letter Of Credit
-Leased Bank Guarantee
-Liquidity Squeeze
-Loan-To-Cost Ratio
-Loan Committee
-Loan Grading
-Loan Strip
-Lockbox Banking
-Loss Given Default
-Merchant Bank
-Money Center Banks
-Money Order}
+COMMON_BILLS = %W{
+ Rent
+ Mortgage
+ Homeowner’s Insurance
+ Renter's Insurance
+ Home Warranty
+ Home Maintenance
+ Home Repairs
+ Lawn Care
+ Property Taxes
+ Car Payment
+ Car Fuel
+ Auto Insurance
+ Car Tires
+ DMV Registration
+ Electric Bill
+ Water Bill
+ Gas Bill
+ Phone Bill
+ Internet Service
+ Cable TV 
+ Satellite TV
+ Groceries
+ Dining out
+ Clothing
+ Hair Care
+ Medical Expenses
+ Gym Membership
+ Vacation
+ Charitable Giving
+ Entertainment
+ Gifts
+}
