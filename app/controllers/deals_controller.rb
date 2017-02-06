b53b1006212d96374745eec8e3696f32e0854ff5
Andrew Arrow <oneone@gmail.com>
2017-02-06 15:53:07 +0000

    mm

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -2,7 +2,17 @@ class DealsController < ApplicationController
 
   def update
     a = Asset.find(params[:id])
-    s = params[:source]
+    a.transaction do
+      if params[:source] == 'c'
+        a.customer.checking += a.value
+      elsif params[:source] == 's'
+        a.customer.savings += a.value
+      elsif params[:source] == 'l'
+        a.customer.loan += a.value
+      end
+      a.destroy
+      a.customer.save
+    end
     redirect_to '/'
   end
 
