b61d5ac663ea4b7f348cb5f403817f1782631758
Andrew Arrow <oneone@gmail.com>
2017-02-06 03:38:55 +0000

    mm

--- app/controllers/kbi/dashboard_controller.rb
+++ app/controllers/kbi/dashboard_controller.rb
@@ -1,5 +1,11 @@
 class Kbi::DashboardController < ApplicationController
   layout 'kbi'
   def welcome
+    @filter = params[:filter]
+    @items = Customer.all
+    if @filter == 'kids'
+      @items = @items.where('dob > ?', 13.years.ago)
+    end
+    @items.order(:id).limit(1000)
   end
 end
