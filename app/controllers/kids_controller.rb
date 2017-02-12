de1f528233105c5ca87aa4ccca77575f94c39d5d
A Arrow <oneone@gmail.com>
2017-02-12 04:23:50 +0000

    fix

--- app/controllers/kids_controller.rb
+++ app/controllers/kids_controller.rb
@@ -13,7 +13,7 @@ class KidsController < ApplicationController
     c=Customer.find_by_id(params[:customer][:name].to_i)
     c=Customer.find_by_email(params[:customer][:email]) unless c
     unless c
-      tokens = params[:customer][:fname].split(' ')
+      tokens = params[:customer][:name].split(' ')
       f = tokens.first
       l = tokens.last
       c=Grownup.create(email: params[:customer][:email],
