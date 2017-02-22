34f3ff120ff15e5cc5f93ac3d9c1891cb2b5d363
A Arrow <oneone@gmail.com>
2017-02-22 19:11:43 +0000

    more

--- iOS/KidBank/SecondViewController.swift
+++ iOS/KidBank/SecondViewController.swift
@@ -7,15 +7,58 @@
 //
 
 import UIKit
+import CoreLocation
 
 class SecondViewController: UIViewController, ARDataSource {
+    
+    fileprivate func getRandomLocation(centerLatitude: Double, centerLongitude: Double, delta: Double) -> CLLocation
+    {
+        var lat = centerLatitude
+        var lon = centerLongitude
+        
+        let latDelta = -(delta / 2) + drand48() * delta
+        let lonDelta = -(delta / 2) + drand48() * delta
+        lat = lat + latDelta
+        lon = lon + lonDelta
+        return CLLocation(latitude: lat, longitude: lon)
+    }
+    
+    fileprivate func getDummyAnnotations(centerLatitude: Double, centerLongitude: Double, delta: Double, count: Int) -> Array<ARAnnotation>
+    {
+        var annotations: [ARAnnotation] = []
+        
+        srand48(3)
+        for i in stride(from: 0, to: count, by: 1)
+        {
+            let annotation = ARAnnotation()
+            annotation.location = self.getRandomLocation(centerLatitude: centerLatitude, centerLongitude: centerLongitude, delta: delta)
+            annotation.title = "POI \(i)"
+            annotations.append(annotation)
+        }
+        return annotations
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-       let arViewController = ARViewController()
-       arViewController.dataSource = self
+        let lat = 45.554833
+        let lon = 18.695433
+        let delta = 0.05
+        let count = 50
+         let dummyAnnotations = self.getDummyAnnotations(centerLatitude: lat, centerLongitude: lon, delta: delta, count: count)
+        
+        let arViewController = ARViewController()
+        arViewController.dataSource = self
+        arViewController.maxDistance = 0
+        arViewController.maxVisibleAnnotations = 100
+        arViewController.maxVerticalLevel = 5
+        arViewController.headingSmoothingFactor = 0.05
+        arViewController.trackingManager.userDistanceFilter = 25
+        arViewController.trackingManager.reloadDistanceFilter = 75
+        arViewController.setAnnotations(dummyAnnotations)
+        arViewController.uiOptions.debugEnabled = true
+        arViewController.uiOptions.closeButtonEnabled = true
     }
     
     func ar(_ arViewController: ARViewController, viewForAnnotation: ARAnnotation) -> ARAnnotationView
