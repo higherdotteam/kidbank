7e3edf56d3af88e2ea3603b999b4018445a5edc9
Andrew Arrow <oneone@gmail.com>
2017-02-22 02:23:21 +0000

    progress

--- iOS/KidBank/FirstViewController.swift
+++ iOS/KidBank/FirstViewController.swift
@@ -18,6 +18,21 @@ class FirstViewController: UIViewController {
          var map: MKMapView?
         map = super.view as! MKMapView?
         NSLog("\(map)")
+        let latitude:CLLocationDegrees = 34.0225
+        
+        let longitude:CLLocationDegrees = 118.5714
+        
+        let latDelta:CLLocationDegrees = 0.05
+        
+        let lonDelta:CLLocationDegrees = 0.05
+        
+        let span = MKCoordinateSpanMake(latDelta, lonDelta)
+        
+        let location = CLLocationCoordinate2DMake(latitude, longitude)
+        
+        let region = MKCoordinateRegionMake(location, span)
+        
+        map!.setRegion(region, animated: false)
     }
 
     override func didReceiveMemoryWarning() {
