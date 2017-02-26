fba0d5778f9c82299d53a341bab5fac9ca96f7f7
Andrew Arrow <oneone@gmail.com>
2017-02-26 22:01:34 +0000

    query

--- app/models/atm_location.rb
+++ app/models/atm_location.rb
@@ -7,4 +7,10 @@ class AtmLocation < ActiveRecord::Base
     r[:h] = heading.to_f
     r
   end
+
+  def self.nearby(lat, lon)
+    sql = "SELECT *, 3956 * 2 * ASIN(SQRT( POWER(SIN((#{lat} - abs( dest.lat)) * pi()/180 / 2),2) + COS(#{lat} * pi()/180 ) * COS( abs (dest.lat) *  pi()/180) * POWER(SIN((#{lon}-dest.lon) *  pi()/180 / 2), 2) )) as distance FROM atm_locations dest order by distance limit 100;"
+    results = AtmLocation.connection.execute(sql)
+    raise results.inspect
+  end
 end
