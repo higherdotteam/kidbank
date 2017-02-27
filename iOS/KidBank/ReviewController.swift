9b6ecff64a2c2afb1ad20d3da8c1cd9734f596a4
A Arrow <oneone@gmail.com>
2017-02-27 03:06:02 +0000

    list

--- iOS/KidBank/ReviewController.swift
+++ iOS/KidBank/ReviewController.swift
@@ -8,6 +8,29 @@
 
 import UIKit
 
-class ReviewController: UITableViewController {
+class ReviewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    @IBOutlet var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: "cell")! as UITableViewCell
+        
+        cell.textLabel?.text = "wfwefwef"
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    private func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
 }
