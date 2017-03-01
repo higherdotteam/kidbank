04ee6a4361708e2151bc3ba600488ea19c4d62c9
Andrew Arrow <oneone@gmail.com>
2017-03-01 06:55:03 +0000

    m

--- app/controllers/api/v1/atms_controller.rb
+++ app/controllers/api/v1/atms_controller.rb
@@ -1,7 +1,16 @@
+require 'fileutils'
 class Api::V1::AtmsController < ApplicationController
 
   def create
-    AtmLocation.create(lat: params[:lat].to_f, lon: params[:lon].to_f, heading: params[:h].to_f)
+    #params[:image].original_filename
+    al=AtmLocation.create(lat: params[:lat].to_f, lon: params[:lon].to_f, heading: params[:h].to_f)
+    data = params[:image].read
+    ids = al.id.to_s
+    p1 = ids[ids.size-1]
+    p2 = ids[ids.size-2]
+    dir = "/www/kidbank/public/images/atms/#{p1}/#{p2}"
+    FileUtils.mkdir_p dir
+    File.open(dir+"/#{ids}", "wb") { |f| f.write(data) }
     render json: {}, status: 200
   end
 
