8e7300d3a2164609cc31c6e6328c175a93455477
A Arrow <oneone@gmail.com>
2017-03-24 17:59:05 +0000

    m

--- iOS/KidBank/CreateViewController.swift
+++ iOS/KidBank/CreateViewController.swift
@@ -3,20 +3,26 @@ import UIKit
 class CreateViewController: UIViewController {
     
     @IBOutlet var username: UITextField!
-    @IBOutlet var phone: UITextField!
     
     @IBOutlet var signupButton: UIButton!
     @IBOutlet var loginButton: UIButton!
     
     
     @IBAction func cancelModal(sender: UIButton) {
-        self.dismiss(animated: true, completion: nil)
+        self.view.endEditing(true)
+        username.isHidden = true
+        self.dismiss(animated: false, completion: nil)
     }
     @IBAction func signup(sender: UIButton) {
         signupButton.isHidden = true
         loginButton.isHidden = true
         
         username.isHidden = false
+        username.becomeFirstResponder()
+        
+        /*
+        Timer.scheduledTimer(timeInterval: 1.0, target: self.username, selector: #selector(becomeFirstResponder), userInfo: nil, repeats: false)
+        */
     }
 
     @IBAction func login(sender: UIButton) {
