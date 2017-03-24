54b5f1246e37f396d04066c1acd3c29426547e4e
A Arrow <oneone@gmail.com>
2017-03-24 17:40:56 +0000

    m

--- iOS/KidBank/CreateViewController.swift
+++ iOS/KidBank/CreateViewController.swift
@@ -2,8 +2,28 @@ import UIKit
 
 class CreateViewController: UIViewController {
     
-    @IBOutlet var email: UITextField!
+    @IBOutlet var username: UITextField!
+    @IBOutlet var phone: UITextField!
     
+    @IBOutlet var signupButton: UIButton!
+    @IBOutlet var loginButton: UIButton!
+    
+    
+    @IBAction func cancelModal(sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func signup(sender: UIButton) {
+        signupButton.isHidden = true
+        loginButton.isHidden = true
+        
+        username.isHidden = false
+    }
+
+    @IBAction func login(sender: UIButton) {
+        signupButton.isHidden = true
+        loginButton.isHidden = true
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
