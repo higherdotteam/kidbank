f3958bcde17b4ef93fd0295627ccb57cc052ac46
A Arrow <oneone@gmail.com>
2017-03-24 18:26:09 +0000

    m

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,12 +1,14 @@
 class Api::V1::CustomersController < ApplicationController
 
   def create
-    c = Customer.where(username: params[:username]).first
+    u = params[:username].downcase
+    c = Customer.where(username: u).first
     if c
       render json: {}, status: 406
       return
     end
-    Customer.create(fname: "Not", lname: "Provided", username: params[:username], dob: 4.years.ago)
+    email = "bad.email#{rand(99999999999999)}@nowhere.com"
+    Customer.create(fname: "Not", lname: "Provided", email: email, username: u, dob: 4.years.ago)
     render json: {}, status: 200
   end
 
