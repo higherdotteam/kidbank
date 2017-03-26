b7158eb866330cb73df97619c1aa82b8b4e72ee2
A Arrow <oneone@gmail.com>
2017-03-26 23:18:05 +0100

    m

--- app/models/atm_event.rb
+++ app/models/atm_event.rb
@@ -1,11 +1,12 @@
 class AtmEvent < ActiveRecord::Base
 
   belongs_to :customer
+  belongs_to :atm_location
 
   def as_json
     r = {}
     r[:id] = id
-    r[:words] = words
+    r[:words] = atm_location.words
     r[:happened_at] = happened_at
     r[:flavor] = flavor
   end
